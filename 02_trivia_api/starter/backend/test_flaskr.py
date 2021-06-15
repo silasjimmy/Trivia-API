cb9a85e490f11d81f93ddcb77529483cbb621bf5
Silas Jimmy <jimmysilas17@gmail.com>
2021-06-15 11:10:58 +0300

    add: Create test cases fro api

--- 02_trivia_api/starter/backend/test_flaskr.py
+++ 02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(
+            'postgres', 'sliman17', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +25,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +34,53 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories(self):
+        response = self.client().get('/categories')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_questions(self):
+        response = self.client().get('/questions')
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_question(self):
+        response = self.client().delete('/questions/5')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_question(self):
+        data = {
+            'question': 'Who is the strongest man alive',
+            'answer': 'no one',
+            'difficulty': 5,
+            'category': 3,
+        }
+
+        response = self.client().post('/questions', json=data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_questions(self):
+        request_data = {
+            'searchTerm': '1930',
+        }
+
+        response = self.client().post('/search/questions', json=request_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_category_questions(self):
+        response = self.client().get('/categories/1/questions')
+        self.assertEqual(response.status_code, 200)
+
+    def test_play_question(self):
+        request_data = {
+            'previous_questions': [5, 9],
+            'quiz_category': {
+                'type': 'History',
+                'id': 4
+            }
+        }
+
+        response = self.client().post('/quizzes', json=request_data)
+        self.assertEqual(response.status_code, 200)
 
 
 # Make the tests conveniently executable
